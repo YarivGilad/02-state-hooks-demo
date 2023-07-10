64110c14278effd640db50d602db589f26e9ce28
Yariv Gilad <yariv.gilad@gmail.com>
2023-07-10 11:45:20 +0300

    modified Counter and UserInput to functions using hooks

--- src/view/Counter.tsx
+++ src/view/Counter.tsx
@@ -1,31 +1,55 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 
 interface Props {
   item: string;
   initial: number;
 }
 
-interface State {
-  count: number;
-}
 
-export class Counter extends Component<Props, State> {
-  state = {
-    count: this.props.initial,
-  };
 
-  addItem = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  render() {
-    const {item} = this.props;
-    return (
-      <div className="hbox space-between mt20">
-        <h3 className="paragraph">{item} counter: {this.state.count}</h3>
-        <button className="button" onClick={this.addItem}>
-          add a {item}
-        </button>
-      </div>
-    );
-  }
+export function Counter(props: Props) {
+  const [count, setCount] = useState<number>(props.initial);
+
+  const { item } = props;
+  return (
+    <div className="hbox space-between mt20">
+      <h3 className="paragraph">
+        {item} counter: {count}
+      </h3>
+      <button className="button" onClick={() => setCount(count + 1)}>
+        add a {item}
+      </button>
+    </div>
+  );
 }
+// import { Component } from "react";
+
+// interface Props {
+//   item: string;
+//   initial: number;
+// }
+
+// interface State {
+//   count: number;
+// }
+
+// export class Counter extends Component<Props, State> {
+//   state = {
+//     count: this.props.initial,
+//   };
+
+//   addItem = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+//   render() {
+//     const {item} = this.props;
+//     return (
+//       <div className="hbox space-between mt20">
+//         <h3 className="paragraph">{item} counter: {this.state.count}</h3>
+//         <button className="button" onClick={this.addItem}>
+//           add a {item}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
--- src/view/UserInput.tsx
+++ src/view/UserInput.tsx
@@ -1,27 +1,37 @@
-import { ChangeEvent, Component } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   question: string;
   placeholder: string;
 }
 
-interface State {
-  output: string;
-}
+export function UserInput({ question, placeholder }:Props) {
 
-export class UserInput extends Component<Props, State> {
-  state = {
-    output: "???",
+  const [output, setOutput] = useState<string>("???"); 
+  const [flag, setFlag] = useState<boolean>(true); 
+  const myInput = useRef<HTMLInputElement>(null);
+
+  useEffect(()=> {
+       console.log('effect used!!! - equivalent to componentDidMount')
+       return ()=> console.log('cleanup code - equivalent to componentWillUnmount');  
+  },[])
+  
+  useEffect(()=> {
+console.log(`effect used!!! 
+after the 'flag' state variable is 
+initialized / changed`);
+  },[flag])
+
+  // const update_answer = (event: ChangeEvent<HTMLInputElement>) => {
+    // const txt = event.target.value
+  const update_answer = () => {
+    const txt = (myInput.current as HTMLInputElement).value
+    setOutput(txt);
+    if(txt.length % 3 === 0){
+      setFlag(!flag);
+    }
   };
 
-  update_answer = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ output: event.target.value });
-  };
-
-  render() {
-
-    const { question, placeholder } = this.props;
-    const { output } = this.state;
 
     return (
       <div className="vbox mt20">
@@ -29,10 +39,11 @@ export class UserInput extends Component<Props, State> {
           <h1 className="paragraph">{question}</h1>
 
           <input
+            ref={myInput}
             type="text"
             className="input cap"
             placeholder={placeholder}
-            onChange={this.update_answer}
+            onChange={update_answer}
           />
         </div>
         <br />
@@ -41,5 +52,50 @@ export class UserInput extends Component<Props, State> {
         </h3>
       </div>
     );
-  }
+  
 }
+// import { ChangeEvent, Component } from "react";
+
+// interface Props {
+//   question: string;
+//   placeholder: string;
+// }
+
+// interface State {
+//   output: string;
+// }
+
+// export class UserInput extends Component<Props, State> {
+//   state = {
+//     output: "???",
+//   };
+
+//   update_answer = (event: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({ output: event.target.value });
+//   };
+
+//   render() {
+
+//     const { question, placeholder } = this.props;
+//     const { output } = this.state;
+
+//     return (
+//       <div className="vbox mt20">
+//         <div className="hbox space-between">
+//           <h1 className="paragraph">{question}</h1>
+
+//           <input
+//             type="text"
+//             className="input cap"
+//             placeholder={placeholder}
+//             onChange={this.update_answer}
+//           />
+//         </div>
+//         <br />
+//         <h3 className="paragraph text-left white-text cap">
+//           Your answer: {output}
+//         </h3>
+//       </div>
+//     );
+//   }
+// }
